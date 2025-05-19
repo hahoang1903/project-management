ace7b15a616323a555744fec33d48be830ede6f8
Ha Hoang <37249195+hahoang1903@users.noreply.github.com>
2025-05-20 01:38:35 +0700

    fix client eslint prettier (#3)
    * fix client eslint prettier
    
    * remove default code

--- client/eslint.config.mjs
+++ client/eslint.config.mjs
@@ -10,7 +10,11 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript", "prettier"),
+  ...compat.extends(
+    "next/core-web-vitals",
+    "next/typescript",
+    "plugin:prettier/recommended",
+  ),
   {
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
