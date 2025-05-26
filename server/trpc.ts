2045a7f444923900016d923fe6d2e734b43a8614
hahoang1903 <duchahoang1903@gmail.com>
2025-05-27 01:00:34 +0700

    correct server

--- server/trpc.ts
+++ server/trpc.ts
@@ -7,6 +7,7 @@ export const createContext = ({
   res,
 }: CreateExpressContextOptions) => ({}); // no context
 type Context = Awaited<ReturnType<typeof createContext>>;
+
 const t = initTRPC.context<Context>().create();
 
 export const router = t.router;
